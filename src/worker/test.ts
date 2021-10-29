a38f434af1d027ff6eaa0ec5cf55de697b8a696d
kotomi.satou <piitun3232@gmail.com>
2021-10-29 09:54:05 +0900

    webworkerを導入

--- src/worker/test.ts
+++
@@ -1,6 +0,0 @@
-const worker: Worker = self as any
-
-worker.addEventListener('message', (event) => {
-  console.log('Worker Recieved:', event.data)
-  worker.postMessage({ result: event.data })
-})
